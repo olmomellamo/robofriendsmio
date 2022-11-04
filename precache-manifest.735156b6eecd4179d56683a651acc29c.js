7103515917edc1abed1fe0cd67f2f5b4765eecc3
olmomellamo <48449893+olmomellamo@users.noreply.github.com>
2022-11-04 19:10:13 +0100

    Updates

---
+++ precache-manifest.735156b6eecd4179d56683a651acc29c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b646727df25321b360f",
+    "url": "/robofriendsmio/static/css/main.6ce38a6c.chunk.css"
+  },
+  {
+    "revision": "2b646727df25321b360f",
+    "url": "/robofriendsmio/static/js/main.2b646727.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriendsmio/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "f2386442eff57872f80a",
+    "url": "/robofriendsmio/static/css/2.3b5618ee.chunk.css"
+  },
+  {
+    "revision": "f2386442eff57872f80a",
+    "url": "/robofriendsmio/static/js/2.f2386442.chunk.js"
+  },
+  {
+    "revision": "987f91f7a865517baf30",
+    "url": "/robofriendsmio/static/js/runtime~main.987f91f7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsmio/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e6311ad9f15c9abce3a9e97796954632",
+    "url": "/robofriendsmio/index.html"
+  }
+];
